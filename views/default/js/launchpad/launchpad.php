7a55c9c52acf96b23b1de6911ec7e760c4209689
Jeff Tilson <jrtilson@gmail.com>
2011-07-06 12:57:01 -0400

    GitX is complaining about whitespace.. fixed it

--- views/default/js/launchpad/launchpad.php
+++ views/default/js/launchpad/launchpad.php
@@ -1,19 +1,19 @@
 <?php
 /**
  * TGS Launchpad JS library
- * 
+ *
  * @package TGSLaunchpad
  * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
  * @author Jeff Tilson
  * @copyright THINK Global School 2010
  * @link http://www.thinkglobalschool.com/
- * 
+ *
  */
 ?>
 //<script>
 elgg.provide('elgg.launchpad');
 
-// Init function 
+// Init function
 elgg.launchpad.init = function() {
 	console.log('launchpad loaded');
 }
